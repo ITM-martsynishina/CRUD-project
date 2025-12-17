39a5e1a20279253561a0846a2a04de4768712d41
martsynishina <kristina-martsynishina@yandex.ru>
2025-12-17 17:19:17 +0300

    Make main empty

--- src/main/java/web/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package web.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id) {
-        super("User with id " + id + " not found");
-    }
-}
